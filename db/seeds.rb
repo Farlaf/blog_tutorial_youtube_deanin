182d4bfb146941adf854e481e4c1d649ef332816
Farlaf <slepuhovoleg@gmail.com>
2022-12-04 22:26:35 +0400

    Aded The Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do |x|
+    Post.create(title: "Title #{x}", body: "Body #{x} a lot of words Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor nemo, quam mollitia harum commodi labore excepturi magni ratione reprehenderit aspernatur voluptatibus facilis, possimus incidunt praesentium placeat quas corrupti obcaecati architecto.")
+end
